9b31afab3bb04e24d570b228e45e7604e5ed2452
Calculator10 <fouedhadded777@gmail.com>
2026-01-08 20:50:00 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'magic-wallpapers-v1';
+const URLS_TO_CACHE = [
+  './',
+  'index.html',
+  'style.css',
+  'favicon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request)).catch(()=>caches.match('index.html'))
+  );
+});
